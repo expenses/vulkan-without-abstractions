a4d35d9a517dd51d5db51477ecd10aec19003ff5
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-03 01:34:51 +0200

    Create logical device

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,24 @@ fn main() {
         // llvmpipe or something might be selected!
         let physical_devices = instance.enumerate_physical_devices().unwrap();
         let physical_device = physical_devices[0];
+
+        let device = instance
+            .create_device(
+                physical_device,
+                &vk::DeviceCreateInfo::default()
+                    // Select a single queue from a single queue family with a priority
+                    // of 1.0. Completely meaningless information when we're just using
+                    // a single queue.
+                    .queue_create_infos(&[
+                        vk::DeviceQueueCreateInfo::default().queue_priorities(&[1.0])
+                    ]),
+                None,
+            )
+            .unwrap();
+
+        // Get the queue, just picking the first one from the first family.
+        // Hopefully these are listed in a sensible order by the driver otherwise
+        // it could choose e.g. a transfer-only queue
+        let queue = device.get_device_queue(0, 0);
     }
 }
